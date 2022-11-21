95cd70707092c1aa1d8a259fc168cfabe8b87449
Paul Taylor <paul@whammydiver.com>
2022-11-21 17:52:38 -0500

    DB connection implemented

--- server/index.js
+++ server/index.js
@@ -1,10 +1,24 @@
+// load .env data into process.env
+require("dotenv").config();
+
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
+const cors = require("cors");
+const morgan = require("morgan");
+
+// PG database client/connection setup
+const { Pool } = require("pg");
+const dbParams = require("./lib/db.js");
+const db = new Pool(dbParams);
+db.connect();
 
 const app = express();
 
-app.get("/api", (req, res) => {
+app.use(cors());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
