df562e2d7b04c1efd8badbf8c59c533bedf7db50
Paul Taylor <paul@whammydiver.com>
2022-11-21 22:25:59 -0500

    first commit

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello from server!" });
+  res.send("message: Hello from server!");
 });
 
 app.listen(PORT, () => {
