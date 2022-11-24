1d2908aa7ca5354baa7270306f1e57ec5daeae13
Paul Taylor <paul@whammydiver.com>
2022-11-24 11:50:44 -0500

    first DB query

--- server/index.js
+++ server/index.js
@@ -7,10 +7,14 @@ const PORT = process.env.PORT || 3001;
 const cors = require("cors");
 const morgan = require("morgan");
 
+const bcrypt = require('bcryptjs');
+const salt = bcrypt.genSaltSync(10);
+
 
 // PG database client/connection setup
 const { Pool } = require("pg");
 const dbParams = require("./lib/db.js");
+const { UserInstance } = require("twilio/lib/rest/conversations/v1/user");
 const db = new Pool(dbParams);
 db.connect();
 
@@ -26,8 +30,20 @@ app.get("/", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  console.log(req.body);
-  res.send("received");
+  db.query(
+    `SELECT * FROM users
+      WHERE email = $1`, [req.body.email])
+      .then((response) => {
+        //console.log(response.rows);
+        // console.log('res.rows[0].password ', response.rows[0].password)
+        // console.log(bcrypt.hashSync(req.body.password, salt));
+        if (bcrypt.compareSync(req.body.password, response.rows[0].password)) {
+          res.send(response.rows[0]);
+        } else {
+          res.send("passwords do not match");
+        };
+      }).catch((err) => console.log(err))
+  // res.send("received");
 })
 
 app.listen(PORT, () => {
