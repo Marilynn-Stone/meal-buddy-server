d3af0fe4ad543b37342daf3a1d019d3be0dcc614
Marilynn Stone <hello@marilynnstone.com>
2022-11-24 13:55:20 -0800

    Add user authentication.

--- server/routes/home.js
+++ server/routes/home.js
@@ -4,7 +4,24 @@ require("dotenv").config();
 
 module.exports = (db) => {
   router.get("/home", (req, res) => {
-    res.send({ message: "Hello from home page!" });
+    // If the user is loggedin
+    if (req.session.loggedin) {
+      // Output username
+      res.send("Welcome back, " + req.session.username + "!");
+    } else {
+      // Not logged in
+      res.send("Please login to view this page!");
+    }
+    res.end();
+
+    //   if (req.session.loggedIn) {
+    //     res.setHeader("Content-Type", "text/html");
+    //     res.write("Welcome " + req.session.username + " to your dashboard");
+    //     res.write('<a href="/logout">Logout</a>');
+    //     res.end();
+    //   } else res.redirect("/login");
+
+    //   res.send({ message: "Hello from home page!" });
   });
 
   return router;
