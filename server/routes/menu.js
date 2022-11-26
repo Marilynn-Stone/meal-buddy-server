9d4b3daf7470f8e333dd28e696c692c55efa1ee4
Marilynn Stone <hello@marilynnstone.com>
2022-11-26 15:11:25 -0800

    Uncomment routes to allow use by React Router.

--- server/routes/menu.js
+++ server/routes/menu.js
@@ -1,21 +1,18 @@
-const express = require('express');
-const router  = express.Router();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+
+require("dotenv").config();
 
 module.exports = (db) => {
-
-
   // if menu exists in DB, call DB
   // else call spoonacular API
-
-
-  // router.get('/menu/:meal', (req, res) => {
-  //   console.log(req.body)
-  //   db.query(`SELECT * FROM menu WHERE user_id = $1;`, [req.params.user.id])
-  //   .then(data => {
-  //     res.send( "data.rows"?? );
-  //   })
-  // })
-
-  // return router;
-
+  router.get("/menu/:meal", (req, res) => {
+    console.log(req.body);
+    db.query(`SELECT * FROM menu WHERE user_id = $1;`, [
+      req.params.user.id,
+    ]).then((data) => {
+      // res.send( "data.rows"?? );
+    });
+  });
+  return router;
 };
