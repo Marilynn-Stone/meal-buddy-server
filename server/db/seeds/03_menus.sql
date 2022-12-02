7579e2923bddeea7c7de2972e77b544825cf2295
Paul Taylor <paul@whammydiver.com>
2022-12-02 12:41:49 -0500

    update DB for demo purposes

--- server/db/seeds/03_menus.sql
+++ server/db/seeds/03_menus.sql
@@ -1,2 +1,5 @@
 INSERT INTO menus (user_id) VALUES
-(1);
+(1),
+(2),
+(3),
+(4);
--- server/routes/menu.js
+++ server/routes/menu.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
+const getRecipeRenderObject = require("../../helperFunctions/getRecipeRenderObject");
 
 require("dotenv").config();
 
@@ -7,16 +8,30 @@ module.exports = (db) => {
 
   router.post("/weekly_menu", (req, res) => {
     console.log(req.body);
-    db.query(`SELECT spoonacular_id, title, day, category FROM meals JOIN menus ON menu_id = menus.id JOIN users ON menus.user_id = users.id WHERE users.id = $1;`, [req.body.user_id,])
+    db.query(
+      `SELECT spoonacular_id, day, category, title AS name FROM meals JOIN menus ON menu_id = menus.id JOIN users ON menus.user_id = users.id WHERE users.id = $1;`,
+      [req.body.user_id]
+    )
       .then((data) => {
+        console.log(JSON.stringify(data.rows.reverse()));
         if (data.rows !== []) {
-        res.send(data.rows);
+          res.send(data.rows);
         }
-      }).catch((err) => {
-         console.log(err);
+      })
+      .catch((err) => {
+        console.log(err);
       });
-    });
+  });
+
+  // router.post("/meal/:id", (req, res) => {
+  //   console.log(req.params);
+  //   app.get(`https://api.spoonacular.com/recipes/${req.params.id}/information`)
+  //   }).then((data) => {
+  //     res.send(getRecipeRenderObject(data));
+  //   }).catch((err) => {
+  //     console.log(err);
+  //   });
 
   return router;
-
 };
+
