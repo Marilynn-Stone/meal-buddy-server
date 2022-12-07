021bed27ea89dc537669a0f5555ccf3141f91a87
Paul Taylor <paul@whammydiver.com>
2022-12-06 19:05:25 -0500

    first guess - new server route to get a new menu

--- server/routes/menu.js
+++ server/routes/menu.js
@@ -81,6 +81,64 @@ module.exports = (db) => {
         })
     });
 
+  router.post("/replace_weekly_menu", (req, res) => {
+    console.log("user ID: ", req.body.user_id);
+    db.query(
+      `DELETE * FROM menus WHERE users.id = $1 RETURNING *;`, [req.body.user_id])
+      .then((user) => {
+      console.log("Deleted Menu Details: ", user.rows[0]);
+      db.query(
+        `SELECT * FROM users JOIN user_diets ON users.id = user_diets.user_id WHERE users.id = $1`,
+        [req.body.user_id])
+        .then((user) => {
+          const userDetails = user.rows[0];
+          const headers = {
+            "Accept-Encoding": "application/json",
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+          }  
+          const params = {
+            parameters: {
+              "username": userDetails.spoonacular_username,
+              "hash": userDetails.spoonacular_hash
+            }
+          }    
+        
+          let restrictionsString = "";
+          for (key of Object.keys(userDetails)) {
+            if (userDetails[key] === true) {
+              restrictionsString += `${key}, `
+            }
+          }
+          if (restrictionsString) {
+            restrictionsString = "&exclude=" + restrictionsString.slice(0, -2);
+          }  
+          
+          let dietString = "";
+          if (userDetails.dietary_category) {
+            dietString = `&diet=${userDetails.dietary_category}`;
+          };
+        
+          axios.get(`https://api.spoonacular.com/mealplanner/generate?apiKey=${process.env.SPOONACULARAPIKEY}&timeFrame=week&targetCalories=${userDetails.caloric_target}${dietString}${restrictionsString}`, {params, headers})
+          .then(async(data) => { 
+            console.log(data.data.meals)
+            return await createWeeklyMenuArray(data.data);
+          }).then(async (menuArray) => {
+            console.log("menuArray: ", menuArray);
+            console.log("length: ", menuArray.length)
+            console.log("user ID: ", req.body.user_id);
+            const menuID = await db.query(`INSERT INTO menus (user_id) VALUES ($1) RETURNING id;`, [req.body.user_id]);
+            console.log("menuID: ", menuID.rows[0].id); 
+            insertMenuIntoDB(menuID.rows[0].id, menuArray)
+            res.send(menuArray);
+          })
+        })
+        .catch((err) => {
+          console.log(err.data);
+        });
+      })
+  });
+    
 
   router.get("/meal/:id", (req, res) => {
     const headers = {
